a2968c67b42f0b43739821b33369614174b5ee50
Kamil-Janik <yaneczq1@gmail.com>
2025-03-18 22:51:34 +0100

    ..

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+    server: {
+      host: '0.0.0.0', // Listen on all network interfaces
+      port: 5173,      // Make sure to use the correct port
+    }
+  };
+  
